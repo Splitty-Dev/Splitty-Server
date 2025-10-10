a21f378ae34828da1a22e8805436e6eb2a284956
chaeyoungeee <chaeinge@gmail.com>
2025-10-10 19:01:47 +0900

    initial commit

---
+++ api/src/main/java/com/chaegangjo/ApiApplication.java
@@ -0,0 +1,14 @@
+package com.chaegangjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackages = "com.chaegangjo")
+@EntityScan(basePackages = "com.chaegangjo")
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
