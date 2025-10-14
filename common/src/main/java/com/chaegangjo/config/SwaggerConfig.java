08c5fbeedddc43ecd4dbfe56e6b8d2e152a79a47
chaeyoungeee <chaeinge@gmail.com>
2025-10-14 23:44:31 +0900

    chore: swagger production server 추가

--- common/src/main/java/com/chaegangjo/config/SwaggerConfig.java
+++ common/src/main/java/com/chaegangjo/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.chaegangjo.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,11 @@ import org.springframework.context.annotation.Configuration;
                 title = "Splitty API",
                 version = "v1.0.0",
                 description = "Splitty API 명세서입니다."
-        )
+        ),
+        servers = {
+                @Server(url = "https://splitty.store", description = "Production Server"),
+                @Server(url = "http://localhost:8080", description = "Local Server")
+        }
 )
 @Configuration
 public class SwaggerConfig {
