aec2edd38b27899a11d92e645c18d008819d014e
chaeyoungeee <chaeinge@gmail.com>
2025-11-01 17:58:45 +0900

    refactor: 알림 내역 클릭 시 리다이렉트 url 변경

--- common/src/main/java/com/chaegangjo/firebase/FirebaseProperties.java
+++ common/src/main/java/com/chaegangjo/firebase/FirebaseProperties.java
@@ -4,6 +4,6 @@ public class FirebaseProperties {
 
     public static final String FIREBASE_SERVICE_ACCOUNT_KEY_PATH = "splitty-firebase-account.json";
     public static final String FCM_REDIS_KEY = "FCM-TOKEN:";
-    public static final String FCM_GOODS_BASE_PATH = "/goods/";
+    public static final String FCM_GOODS_BASE_PATH = "/product/";
     public static final String FCM_CHAT_BASE_PATH = "/chat/";
 }
